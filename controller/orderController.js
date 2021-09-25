9ba0b9ef0826069d87075393d12edf4b36489769
ShahidBangash <shahidbangashsi94@gmail.com>
2021-09-25 23:28:02 +0500

    get all orders done

--- controller/orderController.js
+++ controller/orderController.js
@@ -23,3 +23,29 @@ module.exports.createOrder = async (req, res, next) => {
     });
   }
 };
+
+module.exports.getAllOrders = async (req, res, next) => {
+  try {
+    const orders = await orderModel.find();
+    if (orders == null) {
+      res.status(400).json({
+        success: false,
+        message: "Sorry We have no orders right now",
+        totalOrders: 0,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Here is the list of all orders",
+        totalOrders: orders.length,
+        orders,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "We could not fetch all the orders",
+      error: err,
+    });
+  }
+};
