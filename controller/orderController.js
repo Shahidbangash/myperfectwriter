826934630c0a9955cd285d294756e21b04a1c11f
ShahidBangash <shahidbangashsi94@gmail.com>
2021-09-26 11:33:43 +0500

    leads route added

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const orderModel = require("../model/orderSchema");
+const leadsModel = require("../model/leadsSchema");
 
 module.exports.createOrder = async (req, res, next) => {
   try {
@@ -68,6 +69,33 @@ module.exports.getActiveOrders = async (req, res, next) => {
         message: "Here is the list of all active orders",
         totalOrders: orders.length,
         orders,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "We could not fetch active orders",
+      error: err,
+    });
+  }
+};
+
+// Get leads Orders
+module.exports.getAllLeads = async (req, res, next) => {
+  try {
+    const leads = await leadsModel.find();
+    if (leads == null) {
+      res.status(400).json({
+        success: false,
+        message: "Sorry We have no LEADS right now",
+        totalLeads: 0,
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "Here is the list of all active orders",
+        totalLeads: leads.length,
+        leads,
       });
     }
   } catch (error) {
